2c1689097b195c50a65ccf9e62443cf9a310c193
Dean Johnson <deanjohnson222@gmail.com>
2013-12-17 14:46:41 -0800

    Added settings file.
    Also added twitter settings to go with it.

---
+++ config/settings.rb
@@ -0,0 +1,19 @@
+require 'dashing'
+require "sinatra/config_file"
+
+configure do
+  #set :auth_token, 'YOUR_AUTH_TOKEN'
+
+  set :logging, :true
+
+  # Adds the config to settings
+  config_file 'config.yml'
+
+  helpers do
+    def protected!
+      settings.twitter['consumer_key'] = 'qweqwe'
+     # Put any authentication code you want in here.
+     # This method is run before accessing any resource.
+    end
+  end
+end
--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -1,16 +1,17 @@
 require 'twitter'
-
+require 'sinatra'
 
 #### Get your twitter keys & secrets:
 #### https://dev.twitter.com/docs/auth/tokens-devtwittercom
 twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key = 'YOUR_CONSUMER_KEY'
-  config.consumer_secret = 'YOUR_CONSUMER_SECRET'
-  config.oauth_token = 'YOUR_OAUTH_TOKEN'
-  config.oauth_token_secret = 'YOUR_OAUTH_SECRET'
+  config.consumer_key = settings.twitter['consumer_key']
+  config.consumer_secret = settings.twitter['consumer_secret']
+  config.oauth_token = settings.twitter['oauth_token']
+  config.oauth_token_secret = settings.twitter['oauth_token_secret']
+
 end
 
-search_term = URI::encode('#todayilearned')
+search_term = URI::encode('osuosl')
 
 SCHEDULER.every '10m', :first_in => 0 do |job|
   begin
@@ -22,6 +23,7 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
       end
       send_event('twitter_mentions', comments: tweets)
     end
+
   rescue Twitter::Error
     puts "\e[33mFor the twitter widget to work, you need to put in your twitter API keys in the jobs/twitter.rb file.\e[0m"
   end
