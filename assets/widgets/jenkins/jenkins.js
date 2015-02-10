b07e330d88ea8b6b109cb7368fcd4b2c1ef8ff1e
Lucy Wyman <wyman.lucy@gmail.com>
2015-02-09 17:20:55 -0800

    Jenkins meter finally working!

--- assets/widgets/jenkins/jenkins.js
+++ assets/widgets/jenkins/jenkins.js
@@ -1,4 +1,4 @@
-var dashing = dashing || angular.module('dashing', ['drawing']);
+var dashing = dashing || angular.module('dashing', []);
 
 
 dashing.controller('JenkinsCtrl', ['$scope',
@@ -11,44 +11,11 @@ dashing.controller('JenkinsCtrl', ['$scope',
   }
 ])
 
-/*.directive('canvas', function(){
-  return{
-    restrict: 'AEC',
-    template: "<canvas width='280' height='150' id='jenkins-canvas'></canvas>",
-    scope: {
-      drawing: '=',
-      drawingId: '@' },
-    link: function(scope){
-      scope.canvas = getElementById('jenkins-canvas');
-      scope.scope.context = canvas.getContext('2d');
-      var x = canvas.width / 2;
-      var y = canvas.height / 2;
-      var radius = 50; 
-      var startAngle = 0.8 * Math.PI;
-      var endAngle = 2.2 * Math.PI;
-      var counterClockwise = false;
-
-      scope.context.beginPath();
-      scope.context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
-      scope.context.lineWidth = 30; 
-      scope.context.strokeStyle = '#444';
-      scope.context.stroke();
-      scope.context.endPath();
-
-      scope.context.beginPath();
-      scope.context.arc(x, y, radius, startAngle, endAngle-0.3, counterClockwise);
-      scope.context.lineWidth = 30; 
-      scope.context.strokeStyle = '#fff';
-      scope.context.stroke();
-      scope.context.endPath();
-    }
-  };
-});*/
 
 .directive('jenkinsWidget', function() {
-
   return {
-    restrict: 'AEC',
+    restrict: 'E',
     templateUrl: '/assets/jenkins/jenkins.html'
+  }
 });
 
