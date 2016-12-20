a63bc02d8b0b3ae1cf16ef1a9f9558b974ae8305
Tristan Patch <patcht@osuosl.org>
2016-12-20 12:52:58 -0800

    Commits job was broken during switch to Octokit. It works now.

--- jobs/commits.rb
+++ jobs/commits.rb
@@ -5,32 +5,27 @@ require 'octokit'
 projects = settings.projects || []
 
 SCHEDULER.every '1h' do
-  client = Octokit::Client.new(:access_token => settings.github['token'])
+  client = Octokit::Client.new(access_token: settings.github['token'])
 
-  project = projects.first['name']
-
-  latest_sha = latest_committer = commit_message = commit_date = nil
+  project = projects.first['repo']
 
   # SHA hash
   data = client.ref(project, 'heads/' + projects.first['branch'])
-	latest_sha = data['object']['sha']
+  latest_sha = data['object']['sha']
 
-	# Commit
+  # Commit
   data = client.git_commit(project, latest_sha)
   latest_committer = data['committer']['name']
   commit_message = data['message']
   commit_message = commit_message.split[0...15].join(' ')
-  commit_date = Time.parse(data['committer']['date']).strftime('%a %b %e %Y')
+  commit_date = data['committer']['date'].strftime('%a %b %e %Y')
 
   send_event('commits',
-    {
-      project: {
-        name: name,
-        committer: latest_committer,
-        commit_message: commit_message,
-        date: commit_date
-			}
-    }
-  )
+             project: {
+               name: project,
+               committer: latest_committer,
+               commit_message: commit_message,
+               date: commit_date
+             })
   projects.rotate!
 end
