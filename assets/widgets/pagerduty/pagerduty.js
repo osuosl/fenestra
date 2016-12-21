97c64b2080b97dba26a6559ca2bd34b018957799
Tristan Patch <patcht@osuosl.org>
2016-12-21 15:48:31 -0800

    Fix indentation and rubocop style.

--- assets/widgets/pagerduty/pagerduty.js
+++ assets/widgets/pagerduty/pagerduty.js
@@ -1,18 +1,18 @@
 var dashing = dashing || angular.module('dashing', []);
 
 dashing.controller('PagerdutyCtrl', ['$scope',
-		function ($scope) {
-			'use strict';
+  function ($scope) {
+    'use strict';
 
-			$scope.$on('pagerduty', function (e, data) {
-				angular.extend($scope, data);
-			});
-		}
+    $scope.$on('pagerduty', function (e, data) {
+      angular.extend($scope, data);
+    });
+  }
 ])
 
 .directive('onCall', function() {
-	return {
-		restrict: 'E',
-        templateUrl: '/assets/pagerduty/pagerduty.html'
-	}
+  return {
+    restrict: 'E',
+    templateUrl: '/assets/pagerduty/pagerduty.html'
+  };
 });
